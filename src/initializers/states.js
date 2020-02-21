8a963c88afdf13af40913339257b5e86d67339e1
gabrieelgomez <gagg1707@gmail.com>
2020-02-21 12:07:44 -0400

    add persist redux by local storage

--- src/initializers/states.js
+++ src/initializers/states.js
@@ -1,20 +1,20 @@
-import {fromJS} from 'immutable'
 export const loadState = () => {
   try {
-    const serializedData = localStorage.getItem('state')
-    if (serializedData === null){
-      return undefined // Si no existe el state en el local storage devolvemos undefined para que cargue el state inicial que hayamos definido
+    const serializedState = localStorage.getItem('state');
+    if (serializedState === null) {
+      return undefined;
     }
-    return fromJS(JSON.parse(serializedData)) // Si encontramos con exito nuestro storage lo devolvemos.
-  } catch (error) {
-    return undefined // Si ocurre algun error, devuelvo undefined para cargar el state inicial.
+    return JSON.parse(serializedState);
+  } catch (err) {
+    return undefined;
   }
-}
+};
+
 export const saveState = (state) => {
   try {
-    let serializedData = JSON.stringify(state.toJS())
-    localStorage.setItem('state', serializedData)
-  } catch (error) {
-	// Acá podemos capturar o crear cualquier log que deseemos en caso de que falle el salvado en el storage.
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
+  } catch {
+    // ignore write errors
   }
-}
+};
