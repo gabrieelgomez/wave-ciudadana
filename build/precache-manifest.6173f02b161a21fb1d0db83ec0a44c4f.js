dc9a8f51a45c4ca1159d85336eebba8ef4a945cf
gabrieelgomez <gagg1707@gmail.com>
2020-02-21 12:22:25 -0400

    add yarn build

---
+++ build/precache-manifest.6173f02b161a21fb1d0db83ec0a44c4f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e636d66070bf727d4bd10889cee5b84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9ebf2c7662d6213cc63",
+    "url": "/static/css/2.9ddb28ea.chunk.css"
+  },
+  {
+    "revision": "600bfcc08d10a158b5c5",
+    "url": "/static/css/main.0612847a.chunk.css"
+  },
+  {
+    "revision": "e9ebf2c7662d6213cc63",
+    "url": "/static/js/2.64b82762.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.64b82762.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "600bfcc08d10a158b5c5",
+    "url": "/static/js/main.a48df058.chunk.js"
+  },
+  {
+    "revision": "3ab98dc25bd0b65345f3",
+    "url": "/static/js/runtime-main.7b70a846.js"
+  },
+  {
+    "revision": "3033c062095f6e1952e5e47617248544",
+    "url": "/static/media/bg.3033c062.png"
+  }
+]);
